ac81742ca7a4382de0f49441fefd876e260facbf
Juan Leguizamón <juan.lord96@gmail.com>
2026-01-06 02:07:43 -0500

    otra vez2

--- app.py
+++ app.py
@@ -113,4 +113,4 @@ def actualizar(zona, ahorro, rango_edad, clasif_sbs):
 
 #ejecutar la visual de una manera externa
 
-app.run_server(mode="external")
+app.run_server(host="0.0.0.0", port=8050, debug=True)
